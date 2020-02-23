ff5793e3588efa2d77fd3b81b58ada277ce1ac12
ito.gawa <itoitogawagawa@gmail.com>
2020-02-23 06:00:06 +0000

    follow,unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\User;
 
+use App\Micropost; // 追加
+
 class UsersController extends Controller
 {
     public function index()
@@ -16,5 +18,35 @@ class UsersController extends Controller
             'users' => $users,
         ]);
  
+    }
+    
+    public function followings($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
+    }
+
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,8 +11,10 @@
                     <img class="rounded img-fluid" src="{{ Gravatar::src($user->email, 500) }}" alt="">
                 </div>
             </div>
+            @include('user_follow.follow_button', ['user' => $user])
         </aside>
         <div class="col-sm-8">
+            @include('users.navtabs', ['user' => $user])
             <ul class="nav nav-tabs nav-justified mb-3">
                 <li class="nav-item"><a href="#" class="nav-link">TimeLine</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
